bdc4256316d5f6d1985427eee1ff1dd61098e32d
francescomarino10 <francesco.marino10@studenti.unipa.it>
2022-07-08 18:11:01 +0200

    Buona Fortuna 10

--- funzioni_topic_detection.py
+++ funzioni_topic_detection.py
@@ -1,6 +1,7 @@
-from calendar import c
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.decomposition import TruncatedSVD
+from scipy import sparse
+
 import gensim
 import funzioni_preprocessing_text as fpt
 import itertools
@@ -17,7 +18,7 @@ class LSAModel:
   num_topics : number of topics to be extracted
   n_iter : number of iterations for the LSA algorithm
   """
-  def __init__(self,corpus,num_topics=10,n_iter=300):
+  def __init__(self,corpus,num_topics=10,n_iter=300,cutoff_tf_idf=0):
     #Funzione che presi in input i dati per effettuare l'LSA
     #procede con la pulizia del corpus
     post,self.bigram_mdl=fpt.clear_corpus(corpus)
@@ -26,6 +27,14 @@ class LSAModel:
     #Vettotizzazione del corpus in valori TF-IDF
     self.vectorizer = TfidfVectorizer()
     M=self.vectorizer.fit_transform(lemmatized_bigram_string)
+    M=M.toarray()
+    row,column=M.shape
+    #Eliminazione dei valori TF_IDF sotto una certa soglia 
+    #per ridurre la quantità dei termini da prendere in considerazione e lasciare quelli più significativi
+    for i, j in itertools.product(range(row), range(column)):
+        if M[i][j]<cutoff_tf_idf:
+            M[i][j]=0
+    M=sparse.csr_matrix(M)
     #Dizionario
     self.termini=self.vectorizer.get_feature_names_out().tolist()
     #Applico TruncatedSVD che preso in input la mia matrice documenti-termini,
@@ -86,12 +95,12 @@ class LDAModel:
     post,self.bigram_mdl=fpt.clear_corpus(corpus)
     #crea un dizionario che contine la mappatura di tutti i token del post per il loro id
     self.dictionary = gensim.corpora.Dictionary(post)
-    #converto le parole del dizionario nella sua rappresentazione bag-of-words: una lista di coppie (word_id, word_frequency)
+    #conversione delle parole del dizionario nella sua rappresentazione bag-of-words: una lista di coppie (word_id, word_frequency)
     corpus2 = [self.dictionary.doc2bow(text) for text in post]
     #trasformazione dei dati in un modello vettoriale di tipo TF-IDF
     tfidf = gensim.models.TfidfModel(corpus2)
     self.corpus_tfidf = tfidf[corpus2]
-    #Addestra un modello LDA sul corpus_tfidf precedentemente creato specificando il numero di topic che si vuole restituire 
+    #Addestramento di un modello LDA sul corpus_tfidf precedentemente creato specificando il numero di topic che si vuole restituire 
     self.ldamodel = gensim.models.ldamodel.LdaModel(self.corpus_tfidf, num_topics, id2word=self.dictionary)
   
   #funzione che restituisce tutti i topic relativi al post dato in input come una lista di coppie (topic_id, topic_probability) 
@@ -103,8 +112,8 @@ class LDAModel:
   #funzione che stampa come una stringa la lista di parole associate al topic con probabilità più alta
   def result_TopicLDAMax(self,post):
     result = self.result_TopicLDA(post)
-    massimo = max(result, key=lambda x: x[1])
-    return self.ldamodel.print_topic(massimo, topn=5)
+    massimo = max(result, key=lambda x: x[1])[0]
+    return self.ldamodel.print_topic(massimo, topn=10)
   
   #funzione che stampa tutti i topic con la lista delle parole associate (10 parole per topic)
   def print_topics(self,num_words=10):
@@ -136,18 +145,18 @@ class HDPModel:
     corpus_tfidf = tfidf[corpus2]
     self.hdpmodel = gensim.models.HdpModel(corpus_tfidf, self.dictionary)
 
-  ####(//non riesco a trovare la funzione get_document_topics per HDP, c'è solo get_topics(), forse un errore con LDA?)
+  #funzione che restituisce tutti i topic relativi al post dato in input come una lista di coppie (topic_id, topic_probability)
   def result_TopicHDP(self,post):
     post2 = fpt.post_adapter(post, self.bigram_mdl)
     post3 = self.dictionary.doc2bow(post2)
-    return self.hdpmodel.get_document_topics(post3)
+    return self.hdpmodel[post3]
   
   #funzione che stampa come una stringa la lista di parole associate al topic con probabilità più alta
   def result_TopicHDPMax(self,post):
     result = self.result_TopicHDP(post)
-    massimo = max(result, key=lambda x: x[1])
-    return self.hdpmodel.print_topic(massimo, topn=5)
+    massimo = max(result, key=lambda x: x[1])[0]
+    return self.hdpmodel.print_topic(massimo, topn=10)
   
   #funzione che stampa tutti i topic rilevati con la lista delle parole associate 
-  def print_topics(self,num_words=5):
+  def print_topics(self,num_words=10):
     return self.hdpmodel.print_topics(num_words=num_words)
